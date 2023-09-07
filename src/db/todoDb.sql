7ec940014f36d9c4c7de0ca02d21244eb10b8ea4
Dmitry Aflitonov <aflitonovdv@gmail.com>
2023-09-07 21:49:41 +0300

    Add indices for todo table

--- src/db/todoDb.sql
+++ src/db/todoDb.sql
@@ -13,4 +13,7 @@ CREATE TABLE IF NOT EXISTS todo (
     user_id bigint NOT NUll,
     FOREIGN KEY (list_id)
       REFERENCES list (list_id)
-);
+)
+
+CREATE INDEX list_idx ON todo (list_id);
+CREATE INDEX user_idx ON todo (user_id);
